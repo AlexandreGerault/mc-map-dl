6a50341a2f2014d1f6298e9abb61a78115114d02
Alexandre Gérault <alexandre@hexium.io>
2021-02-10 16:26:28 +0100

    chore: remove unused import

--- app.ts
+++ app.ts
@@ -1,7 +1,7 @@
 import { serve } from "https://deno.land/std@0.86.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.86.0/http/file_server.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
-import { JSZip, zipDir } from "https://deno.land/x/jszip/mod.ts";
+import { zipDir } from "https://deno.land/x/jszip/mod.ts";
 import { exists } from "https://deno.land/std@0.86.0/fs/mod.ts";
 import { v4 } from "https://deno.land/std@0.86.0/uuid/mod.ts";
 
