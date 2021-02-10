388ef02c1d072fbda3a5b6e4b428f7cf845d2a94
Alexandre Gérault <alexandre@hexium.io>
2021-02-10 16:25:24 +0100

    chore: remove unused code

--- app.ts
+++ app.ts
@@ -9,55 +9,10 @@ import "https://deno.land/x/dotenv/load.ts";
 
 const { MINECRAFT_SERVER_PATH: srcFolder, PORT, HOSTNAME } = config();
 
-const server = serve({ hostname: HOSTNAME, port: parseInt(PORT, 10) });
-console.log(
-  `HTTP webserver running.  Access it at:  http://${HOSTNAME}:${PORT}/`,
-);
-
-const zipper = () => {
-  const zip = new JSZip();
-
-  const add = async (entry: Deno.DirEntry, path?: string) => {
-    const genPath = (prefix: string, subject: Deno.DirEntry) => {
-      return `${prefix}${subject.name}`;
-    };
-
-    zip.folder(path + "/");
-    if (entry.isFile) {
-      const p = genPath(path + "/", entry);
-      console.log(`~add():${p}`);
-      zip.addFile(
-        entry.name,
-        await Deno.readFile(p),
-      );
-    } else if (entry.isDirectory) {
-      const p = genPath(path + "/", entry);
-      console.log(`~add():${p}`);
-      for await (
-        const innerEntry of Deno.readDir(
-          p,
-        )
-      ) {
-        const q = genPath(path + "/", entry);
-        console.log(`~add():${q}`);
-        await add(innerEntry, q);
-      }
-    }
-  };
-
-  const folder = async (path: string) => {
-    for await (const dirEntry of Deno.readDir(srcFolder)) {
-      console.log(`~folder():${srcFolder}/${dirEntry.name}`);
-      await add(dirEntry, path);
-    }
-  };
-
-  const getZip = () => {
-    return zip;
-  };
-
-  return { getZip, folder };
-};
+const server = serve({
+  hostname: HOSTNAME ?? "127.0.0.1",
+  port: parseInt(PORT, 10) ?? 1234,
+});
 
 for await (const request of server) {
   switch (request.url) {
@@ -86,7 +41,6 @@ for await (const request of server) {
 
     case "/": {
       const body = await Deno.readTextFile(`${Deno.cwd()}/public/index.html`);
-      /* const body = read html file content */
       request.respond({ status: 200, body });
       break;
     }
